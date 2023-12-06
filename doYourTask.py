d2eccc2433a1ae26ebf3a24a8286602986a27ebf
夏純心 <junshin@XiaTianMacBook-Air.local>
2023-12-07 01:35:02 +0900

    a file uploaded

---
+++ doYourTask.py
@@ -0,0 +1,22 @@
+import os,schedule,time,webbrowser,datetime
+import subprocess
+
+timeVal="16:35"
+url="https://XXXXXXXXXXXX.jp"
+
+def command():subprocess.run(['ls'])
+command()
+
+def task():
+    webbrowser.open(url,new=1,autoraise=True)
+    webbrowser.get('Safari')
+schedule.every().day.at(timeVal).do(task)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+#example of crontab configuration
+"""
+0 9 * * * /Users/UserName/.venv/bin/python /Users/UserName/DirectoryName/doYourTask.py
+# echo
+"""
