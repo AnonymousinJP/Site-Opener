7fbd824d79bfc4fecc1f4740bd8a44e8fae509ed
AnonymousinJP <wp360sfs@gmail.com>
2025-04-19 18:39:50 +0900

    複数の時間とurlを指定できるようにしました

--- doYourTask.py
+++ doYourTask.py
@@ -1,22 +1,20 @@
-import os,schedule,time,webbrowser,datetime
+import os,schedule,time,webbrowser,datetime,random
 import subprocess
-
-timeVal="16:35"
-url="https://XXXXXXXXXXXX.jp"
-
-def command():subprocess.run(['ls'])
-command()
-
-def task():
-    webbrowser.open(url,new=1,autoraise=True)
-    webbrowser.get('Safari')
-schedule.every().day.at(timeVal).do(task)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-#example of crontab configuration
-"""
-0 9 * * * /Users/UserName/.venv/bin/python /Users/UserName/DirectoryName/doYourTask.py
-# echo
-"""
+class DoTasks:
+    def __init__(self,time,url):self.time=time;self.url=url
+    def task(self):
+        webbrowser.open(self.url,new=1,autoraise=True)
+        webbrowser.get('Safari')
+        schedule.every().day.at(self.time).do(self.task)
+        while True:schedule.run_pending();time.sleep(1);return #returnでループ終了
+doTasks0=DoTasks("00:00","https://任意のurl") #任意の時間, 任意のurl
+doTasks1=DoTasks("00:00","https://任意のurl")
+doTasks2=DoTasks("00:00","https://任意のurl")
+def randNum():
+    num=random.randrange(0,4) #乱数
+    if num==0:doTasks0.task()
+    elif num==1:doTasks1.task()
+    elif num==2:doTasks2.task()
+    elif num==3:doTasks2.task()
+    else:pass
+randNum()
